ff53943457799e3cab7b043e2ff89d5406595cf5
Hainuo Wang <wanghainuo1@gmail.com>
2026-07-07 12:15:46 +0100

    Create Preproc.R

---
+++ Preproc.R
@@ -0,0 +1,678 @@
+
+rm(list= ls())
+
+
+# if('devtools' %in% rownames(installed.packages())==FALSE){
+#   install.packages('devtools')
+#   library(devtools)
+# }else{
+#   library(devtools)
+# }
+# install_github('martin-vasilev/EMreading')
+
+library(EMreading)
+
+
+##Question accuracy:
+quest<- Question(data_list = 'C:/Data/corr_sacc/padded',
+                maxtrial = 90)
+quest <- read.csv("quest.csv")
+
+write.csv(quest, file= 'data/quest.csv')
+
+
+trial_time<- trialTime(data_list = 'C:/Data/corr_sacc/padded',
+                       maxtrial = 90)
+trial_time <- read.csv("trial_time.csv")
+
+write.csv(trial_time, file = 'data/trial_time.csv')
+
+
+
+
+# Get fixation data from raw file and combine with Eyedoctor:
+
+raw_fix<- preprocFromDA1(data_dir = 'C:/Data/corr_sacc/asc',
+                      maxtrial = 90, tBlink = 150, padding = 5)
+
+write.csv(raw_fix, file = 'data/raw_fixations.csv')
+raw_fix <-read.csv("raw_fixations.csv")
+
+# DC<- ExtractMessages(data_list = 'C:/Data/corr_sacc/padded', 
+#                      maxtrial = 90,   message_name =  
+#                       c('DISPLAY CHANGE STARTED',
+#                         'DISPLAY CHANGE COMPLETED'))
+# 
+# #DC$flag_time<- NA
+# DC$flag<- NA
+# 
+# for(i in 1:nrow(DC)){
+#   
+#   msg<- unlist(strsplit(DC$whole_message[i], ' '))
+#   time<- msg[1]
+#   change<- msg[4] 
+#   # DC$flag_time[i]<- as.numeric(unlist(strsplit(time, '\t'))[2])
+#    DC$flag[i]<- change
+# }
+# 
+# DC$message<- NULL
+# DC$whole_message<- NULL
+
+
+DC<- Boundary(data_list = 'C:/Data/corr_sacc/padded', boundary_loc = 618, maxtrial = 90)
+
+# save display change data frame:
+write.csv(DC, file = 'data/display_changes.csv')
+
+
+##############################
+# Preprocessing of raw data: #
+##############################
+
+# See pre-registered protocol for data preprocessing criteria:
+# https://osf.io/9sngw#analysis-plan.data-exclusion
+
+#load("preproc/raw_fix.Rda")
+
+raw_fix <- read.csv("~/R/Jitter/data/raw_fixations.csv")
+raw_fix <-read.csv("raw_fixations.csv")
+raw_fix$hasText<- NULL
+
+
+#######################################
+# first, let's code some new variables:
+
+raw_fix_new<- NULL
+
+raw_fix$prev_RS<- NA
+raw_fix$next_RS<- NA
+raw_fix$prevChar<-NA
+raw_fix$nextChar<- NA
+raw_fix$prevX<- NA
+raw_fix$nextX<- NA
+raw_fix$prevY<- NA
+raw_fix$prev_max_char_line<- NA
+raw_fix$prevEFIX<- NA
+
+nsubs<- unique(raw_fix$sub)
+
+for(i in 1:length(nsubs)){
+  n<- subset(raw_fix, sub==nsubs[i])
+  nitems<- unique(n$item)
+  cat(i); cat(" ")
+  
+  for(j in 1:length(nitems)){
+    m<- subset(n, item== nitems[j])
+    
+    l1<- subset(m, line==1)
+    max_l1<- l1$max_char_line[1]
+    
+    for(k in 1:nrow(m)){
+      if(k==1){
+        m$prev_RS[k]<- 0
+        m$next_RS[k]<- 0
+        m$next_RS[k+1]<- 0
+        
+        ####
+        m$nextChar[k]<- m$char_line[k+1] # next char
+        m$nextX[k]<- m$xPos[k+1]
+        
+      }else{
+        
+        if(is.na(m$SFIX[k])){
+          next
+        }
+        
+        if(m$Rtn_sweep[k]==1){
+          m$prev_RS[k-1]<- 1
+          
+          if(k+1 <= nrow(m)){
+            m$next_RS[k+1]<- 1
+          }
+          
+        }else{
+          m$prev_RS[k-1]<- 0
+          
+          if(k+1 <= nrow(m)){
+            m$next_RS[k+1]<- 0
+          }
+        }
+        ###
+        m$prevChar[k]<- m$char_line[k-1] # prev char
+        m$prevX[k] <- m$xPos[k-1] # prev x
+        m$prevY[k]<- m$yPos[k-1]
+        m$prevEFIX[k]<- m$EFIX[k-1]
+        
+        if(k+1<= nrow(m)){
+          m$nextChar[k]<- m$char_line[k+1] # next char
+          m$nextX[k]<- m$xPos[k+1] # next x
+        }
+        
+        
+      }
+      
+      if(k== nrow(m)){
+        m$prev_RS[k]<- 0
+      }
+      
+      ## map previous line length (for launch site calculation):
+      if(!is.na(m$line[k])){
+        if(m$line[k]==2){
+          m$prev_max_char_line[k]<- max_l1
+        }else{
+          m$prev_max_char_line[k]<- NA
+        }
+      }else{
+        if(m$Rtn_sweep[k]==1){
+          m$prev_max_char_line[k]<- max_l1
+        }
+      }
+      
+    } # end of m
+    raw_fix_new<- rbind(raw_fix_new, m)
+  } # end of j
+  
+  
+}
+
+raw_fix<- raw_fix_new;
+rm(raw_fix_new)
+
+
+
+nAllTrials<- length(nsubs)*90
+
+########################################
+# check number of trials per subject
+nTrials<- NULL
+
+for(i in 1:length(nsubs)){
+  n<- subset(raw_fix, sub== nsubs[i])
+  nTrials[i]<- length(unique(n$item))
+}
+nTrials
+
+# 3 trials were discarded during manual processing (due to track losses, etc.)
+
+nDiscardedTrials<- nAllTrials- sum(nTrials)
+
+
+##############################
+# check if there is only 1 return sweep per trial (expected, due to the two lines):
+
+noRS_sub<- NULL # no return sweeps this trial
+noRS_item<- NULL # no return sweeps this trial
+for(i in 1:length(nsubs)){
+  n<- subset(raw_fix, sub==nsubs[i])
+  nitems<- unique(n$item)
+  
+  for(j in 1:length(nitems)){
+    m<- subset(n, item== nitems[j] & !is.na(SFIX))
+    
+    if(sum(m$Rtn_sweep)>1){ # >1 RS detected..
+      cat(sprintf("Subject %g, item %g, %g return sweeps\n", i, nitems[j], sum(m$Rtn_sweep)))
+    }
+    
+    if(sum(m$Rtn_sweep)==0){ # no return sweeps detected
+      cat(sprintf("Subject %g, item %g, %g return sweeps\n", i, nitems[j], sum(m$Rtn_sweep)))
+      noRS_sub<- c(noRS_sub, i)
+      noRS_item<- c(noRS_item, nitems[j])
+      
+    }
+  }
+}
+
+
+if(length(noRS_sub)>0){
+  
+  # remove trials with no return sweeps:
+  for(i in 1:length(noRS_sub)){
+    out<- which(raw_fix$sub== noRS_sub[i]& raw_fix$item== noRS_item[i])
+    raw_fix<- raw_fix[-out,]
+    
+  }
+  
+}
+
+
+nNoRS<- length(noRS_sub)
+
+#########################
+# let's merge fixations smaller than 80 ms
+install.packages("EMreading")
+
+raw_fix_new<- cleanData(raw_fix, removeOutsideText = F, removeBlinks = F, combineNearbySmallFix = T, 
+                        combineMethod = "char", combineDist = 1, removeSmallFix = F, 
+                        removeOutliers = F, keepRS = T)
+raw_fix<- raw_fix_new
+rm(raw_fix_new)
+
+less80<- raw_fix[which(raw_fix$fix_dur<80 & raw_fix$Rtn_sweep==1), ]
+
+
+# remove remaining fixations less than 80 only if they are not return sweeps:
+#raw_fix<- raw_fix[which(raw_fix$fix_dur<80 & raw_fix$Rtn_sweep==1), ]
+raw_fix<- raw_fix[-which(raw_fix$fix_dur<80), ]
+
+# Some of the discarded <80 ms fixations may be return sweep ones. Therefore, we will remap fixations for such trials,
+# by taking the next fixation as the return sweep one
+
+nsubs<- unique(raw_fix$sub)
+
+new<- NULL
+for(i in 1:length(nsubs)){ # for each subject..
+  n<- subset(raw_fix, sub== nsubs[i])
+  nitems<- unique(n$item)
+  
+  for(j in 1:length(nitems)){ # for each item..
+    m<- subset(n, item== nitems[j])
+    
+    if(sum(m$Rtn_sweep[which(!is.na(m$SFIX))])>0){
+      new<- rbind(new, m)
+    }else{
+      line= 1
+      for(k in 1:nrow(m)){
+        if(!is.na(m$line[k])){
+          if(m$line[k]> line){
+            line= line+1
+            m$Rtn_sweep[k]<- 1
+            if(m$xPos[k+1]> m$xPos[k]){
+              m$Rtn_sweep_type[k]<- "accurate" 
+            }else{
+              m$Rtn_sweep_type[k]<- "undersweep"
+            }
+            
+            a<- which(less80$sub== m$sub[1] & less80$item== m$item[1])
+            if(length(a)>0){
+              if(!is.na(less80$char_line[a])){
+                m$prevChar[k]<- less80$prevChar[a]
+                m$prevX[k]<- less80$prevX[a]
+                m$prevY[k]<- less80$prevY[a] 
+              }else{
+                m$prevChar[k]<- m$char_trial[k-1]
+                m$prevX[k]<- m$xPos[k-1]
+                m$prevY[k]<- m$yPos[k-1] 
+              }
+              
+            }else{
+              m$prevChar[k]<- m$char_trial[k-1]
+              m$prevX[k]<- m$xPos[k-1]
+              m$prevY[k]<- m$yPos[k-1] 
+            }
+            
+          }
+        }
+      } 
+      new<- rbind(new, m)
+      cat(sprintf('changed sub %g item %g \n', m$sub[1], m$item[1]))
+    }
+    
+  }
+  cat(i); cat("\n")
+}
+
+raw_fix<- new; rm(new)
+
+
+### double-check to make sure there are no trials with 0 RS..
+noRS_sub<- NULL # no return sweeps this trial
+noRS_item<- NULL # no return sweeps this trial
+for(i in 1:length(nsubs)){
+  n<- subset(raw_fix, sub==nsubs[i])
+  nitems<- unique(n$item)
+  
+  for(j in 1:length(nitems)){
+    m<- subset(n, item== nitems[j]& !is.na(SFIX))
+    
+    if(sum(m$Rtn_sweep)>1){ # >1 RS detected..
+      cat(sprintf("Subject %g, item %g, %g return sweeps\n", i, nitems[j], sum(m$Rtn_sweep)))
+    }
+    
+    if(sum(m$Rtn_sweep)==0){ # no return sweeps detected
+      cat(sprintf("Subject %g, item %g, %g return sweeps\n", i, nitems[j], sum(m$Rtn_sweep)))
+      noRS_sub<- c(noRS_sub, i)
+      noRS_item<- c(noRS_item, nitems[j])
+      
+    }
+  }
+}
+
+# # one more trial is removed as there are no more fixations on the second line
+# for(i in 1:length(noRS_sub)){
+#   out<- which(raw_fix$sub== noRS_sub[i]& raw_fix$item== noRS_item[i])
+#   raw_fix<- raw_fix[-out,]
+#   
+# }
+# nNoRS<- nNoRS+ length(noRS_item)
+
+
+##########################
+# check for blinks occuring on return sweep saccade/fixation:
+RS_blinks<- raw_fix[which(raw_fix$Rtn_sweep==1), ]
+RS_blinks<- subset(RS_blinks, blink==1)
+
+nBlinks<- nrow(RS_blinks)
+
+if(nBlinks> 0){
+  for(i in 1:nrow(RS_blinks)){
+    raw_fix<- raw_fix[-which(raw_fix$sub== RS_blinks$sub[i]& raw_fix$item== RS_blinks$item[i]),]
+  }
+  
+}
+
+
+# remove also blinks that did not occur next to return sweeps:
+raw_fix<- raw_fix[-which(raw_fix$blink==1),]
+
+
+# confirm we got the correct num of trials:
+RS_blinks<- raw_fix[which(raw_fix$Rtn_sweep==1), ]
+nrow(RS_blinks)== nAllTrials- nBlinks- nNoRS- nDiscardedTrials
+
+RS_blinks<- subset(RS_blinks, blink==1)
+
+if(nrow(RS_blinks)==0){
+  cat("GOOD!")
+  rm(RS_blinks)
+}else{
+  cat(":(")
+}
+
+table(raw_fix$blink)
+
+# remove blink columns
+raw_fix$blink<- NULL
+raw_fix$prev_blink<- NULL
+raw_fix$after_blink<- NULL
+
+
+####################
+# check for outliers:
+
+outliers<- raw_fix[which(raw_fix$fix_dur>1000),]
+
+outliers<- subset(outliers, Rtn_sweep==1 | prev_RS==1 | next_RS==1)
+
+nOutliers<- nrow(outliers)
+
+if(nOutliers>0){
+  
+  for(i in 1:nrow(outliers)){
+    raw_fix<- raw_fix[-which(raw_fix$sub== outliers$sub[i] & raw_fix$item== outliers$item[i]), ]
+  }
+  
+}
+
+
+remove_blink<- which(raw_fix$fix_dur>1000)
+
+if(length(remove_blink)>0){
+  # remove remaining outlier fixations (not next to return sweeps)
+  raw_fix<- raw_fix[-which(raw_fix$fix_dur>1000),]
+  
+}
+
+outOfBnds<- which(raw_fix$outOfBnds==1 & raw_fix$Rtn_sweep==0 & raw_fix$prev_RS==0 & raw_fix$next_RS==0)
+
+if(length(outOfBnds)){
+  # remove fixations outside screen bounds:
+  raw_fix<- raw_fix[-outOfBnds,]
+}
+
+
+# not really sure why we still have these, but we remove them here:
+outsideLeft<- which(raw_fix$prevX<0 |raw_fix$nextX<0)
+
+if(length(outsideLeft)){
+  raw_fix<- raw_fix[-outsideLeft,]
+}
+
+
+###################################################
+# let's verify we have the correct number of trials:
+RS<- subset(raw_fix, Rtn_sweep==1)
+
+nrow(RS)+ nOutliers+ nBlinks+ nNoRS+ nDiscardedTrials == nAllTrials
+
+
+### Check display changes:
+
+library(readr)
+library(tidyverse)
+DC <- read_csv("data/display_changes.csv")
+
+RS<- RS %>%
+  inner_join(DC, by= c('sub', 'item', 'cond'))
+
+# check display change occured within return-sweep:
+RS$DS_in_RS<- ifelse(RS$tStarted>= RS$prevEFIX & RS$tCompleted<= (RS$SFIX+10),1 ,0)
+
+
+
+
+
+# now let's print a summary:
+
+fileConn<- file("preprocessing/Preproc_summary.txt", "w")
+
+writeLines(sprintf("- %#.2f percent of trials manually discarded due to tracking loss, etc.",  
+                   round((nDiscardedTrials/nAllTrials)*100, 2)), fileConn)
+
+writeLines(sprintf("- %#.2f percent of trials discarded due to the lack of a return sweep in the trial",  
+                   round((nNoRS/nAllTrials)*100, 2)), fileConn)
+
+writeLines(sprintf("- %#.2f percent of trials discarded due to blinks on or around return sweeps",  
+                   round((nBlinks/nAllTrials)*100, 2)), fileConn)
+
+writeLines(sprintf("- %#.2f percent of trials discarded due to outliers",  
+                   round((nOutliers/nAllTrials)*100, 2)), fileConn)
+
+writeLines(sprintf("- %#.2f percent of trials remaining for analysis",  
+                   round((nrow(RS)/nAllTrials)*100, 2)), fileConn)
+
+close(fileConn)
+
+
+###################
+# re-organise file:
+raw_fix$sent<- NULL # all items are 1 sentence
+raw_fix$outOfBnds<- NULL # remove above
+raw_fix$time_since_start<- NULL # no longer necessary
+raw_fix$prev_RS<- NULL
+raw_fix$next_RS<- NULL
+
+
+
+
+######## dependent variables analysis
+#DV1: corrective saccade probability (Bernoulli GLMM)
+
+library(tidyverse)
+
+rs  <- read.csv("return-sweep_data.csv")
+qst <- read.csv("quest.csv")              
+fix <- read.csv("raw_fixations.csv")     
+
+
+#calculate the pixels per character: 18.1 pixel/xharacter, 0.337 deg/char
+l1  <- fix %>% filter(!is.na(SFIX), line == 1, outsideText == 0, !is.na(char_line))
+PXC <- unname(coef(lm(xPos ~ char_line, data = l1))["char_line"])
+
+
+dat <- rs %>%
+  transmute(
+    sub, item, cond,
+    corr_sacc_prob = undersweep_prob,
+
+    #   DV2 = landStart_RS + (x_corrective - x_RS) / PXC -> the corrective saccade exceeds the character margin
+    corr_sacc_land_pos = if_else(undersweep_prob == 1,
+                                 landStart + (nextX - xPos) / PXC,
+                                 NA_real_),
+    
+    launch_site_raw = launchSite,
+    # "return-sweep landing position" covariate = line-relative
+    #   landStart (how far into line 2 the RS landed), NOT the word-relative
+    #   land_pos column. Confirm。
+    land_pos_raw    = landStart,
+    DS_in_RS
+  )
+
+
+#relevel and dummy coding
+dat$cond <- factor(dat$cond, levels = c(1, 2, 3),
+                   labels = c("normal", "left", "right"))
+dat$cond <- relevel(dat$cond, ref = "normal")   
+
+
+#centering the covariates
+dat$launch_site <- as.numeric(scale(dat$launch_site_raw, center = TRUE, scale = FALSE))
+dat$land_pos    <- as.numeric(scale(dat$land_pos_raw,    center = TRUE, scale = FALSE))
+
+
+##exclusion criteria
+n_start <- nrow(dat)
+#Display change must have occurred within the return-sweep window,
+#not triggered outside the RS saccade, or completed >10 ms after the line-2 started
+dat <- dat %>% filter(DS_in_RS == 1)
+
+#Participant comprehension accuracy < 70% -> drop participant
+acc      <- qst %>% group_by(sub) %>% summarise(acc = mean(accuracy, na.rm = TRUE))
+keep_sub <- acc %>% filter(acc >= 0.70) %>% pull(sub)
+dat      <- dat %>% filter(sub %in% keep_sub)
+
+# Missing data: drop participants retaining < 50% of experimental trials.
+retained <- dat %>% filter(item %in% 1:90) %>% count(sub, name = "n_kept")
+drop_sub <- retained %>% filter(n_kept < 0.50 * 90) %>% pull(sub)
+dat      <- dat %>% filter(!(sub %in% drop_sub))
+
+
+dat$sub  <- factor(dat$sub)
+dat$item <- factor(dat$item)
+
+
+
+
+dat_prob <- dat                                            # DV1: all valid RS
+dat_land <- dat %>% filter(!is.na(corr_sacc_land_pos))     # DV2: undersweeps only
+
+
+cat(sprintf("px/char used            : %.2f\n", PXC))
+cat(sprintf("RS before exclusions    : %d\n", n_start))
+cat(sprintf("RS after exclusions     : %d  (DV1 / dat_prob)\n", nrow(dat_prob)))
+cat(sprintf("Undersweeps (DV2 subset): %d  (dat_land)\n", nrow(dat_land)))
+cat("\nP(corrective saccade) by condition (DV1):\n")
+print(dat_prob %>% group_by(cond) %>%
+        summarise(p = mean(corr_sacc_prob), n = n()))
+cat("\nCorrective landing position by condition (DV2, chars):\n")
+print(dat_land %>% group_by(cond) %>%
+        summarise(mean = mean(corr_sacc_land_pos),
+                  sd = sd(corr_sacc_land_pos), n = n()))
+
+
+##### corrective saccade probability -- Buenoulli GLMM
+library(tidyverse)
+library(brms)
+
+# 1 = control, 2 = left, 3 = right.
+
+dat_prob$cond <- factor(dat_prob$cond,
+                        levels = c("normal", "left", "right"))  # 'normal' = control
+dat_prob$cond <- relevel(dat_prob$cond, ref = "normal")
+
+f_prob <- bf(corr_sacc_prob ~ cond + launch_site + land_pos +
+               (cond | sub) + (cond | item))
+
+get_prior(f_prob, data = dat_prob, family = bernoulli())
+#NAs： delete or filter ？？？
+colSums(is.na(dat_prob[, c("corr_sacc_prob","launch_site","land_pos")]))
+dat_prob[is.na(dat_prob$launch_site), 
+         c("sub","item","cond","corr_sacc_prob","launch_site_raw")]
+
+
+priors_prob <- c(
+  set_prior("normal(0, 2)",    class = "b", coef = "condleft"),    
+  set_prior("normal(0, 2)",    class = "b", coef = "condright"),   
+  set_prior("normal(-0.3, 1)", class = "b", coef = "launch_site"), 
+  set_prior("normal(0, 2)",    class = "b", coef = "land_pos"),
+  set_prior("normal(0.6, 1)",  class = "Intercept")                
+)
+
+# ---- ---------------------------------------------------------------
+m_prob <- brm(
+  formula      = f_prob,
+  data         = dat_prob,
+  family       = bernoulli(),       
+  prior        = priors_prob,
+  sample_prior = TRUE,               
+  warmup       = 1000,
+  iter         = 6000,
+  chains       = 4,
+  cores        = 4,
+  seed         = 1234,
+  control      = list(adapt_delta = 0.95)  # separation -> reduce divergences
+)
+
+
+summary(m_prob)                
+plot(m_prob)                   
+pp_check(m_prob, ndraws = 100) 
+
+# Inspect the condition contrasts (this is the DV1 result):
+fixef(m_prob)[c("condleft", "condright"), ]
+
+
+#####landing position -- Guassion GLMM
+
+dat_land$cond <- factor(dat_land$cond, levels = c("normal", "left", "right"))
+dat_land$cond <- relevel(dat_land$cond, ref = "normal")
+
+f_land <- bf(corr_sacc_land_pos ~ cond + launch_site + land_pos +
+               (cond | sub) + (cond | item))
+
+get_prior(f_land, data = dat_land, family = gaussian())
+
+priors_land <- c(
+  set_prior("normal(0, 2)", class = "b", coef = "condleft"),    
+  set_prior("normal(0, 2)", class = "b", coef = "condright"),  
+  set_prior("normal(0, 1)", class = "b", coef = "launch_site"),
+  set_prior("normal(0, 2)", class = "b", coef = "land_pos"),
+  set_prior("normal(4, 2)", class = "Intercept") 
+)
+
+
+m_land <- brm(
+  formula      = f_land,
+  data         = dat_land,
+  family       = gaussian(),
+  prior        = priors_land,
+  sample_prior = TRUE,              
+  warmup       = 1000,
+  iter         = 6000,
+  chains       = 4,
+  cores        = 4,
+  seed         = 1234,
+  control      = list(adapt_delta = 0.95)
+)
+
+
+summary(m_land)                 
+plot(m_land)                   
+pp_check(m_land, ndraws = 100)  
+
+# The DV2 result — the condition contrasts on landing position:
+fixef(m_land)[c("condleft", "condright"), ]
+
+prior_summary(m_land)
+
+
+### additional: latency
+
+
+
+
+
+
+
+
+
+
